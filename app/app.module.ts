196bfa96a3ec49b9aa3bce4a67eaba8595b0e70a
Stephen Njoroge <steve254@Stephens-MacBook-Pro.local>
2017-01-06 11:26:30 +0300

    Add star component to app module

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms'
 import { AppComponent }  from './app.component';
 import { ProductListComponent } from './products/product-list.component'
 import { ProductFilterPipe } from './products/product-filter.pipe'
+import { StarComponent } from './shared/star.component'
 
 @NgModule({
   imports: [ 
@@ -13,6 +14,7 @@ import { ProductFilterPipe } from './products/product-filter.pipe'
     AppComponent, 
     ProductListComponent,
     ProductFilterPipe,
+    StarComponent,
     ],
   bootstrap: [ AppComponent ]
 })
